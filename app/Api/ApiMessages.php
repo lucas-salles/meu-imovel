a20bca90c9d4e3fab54308f28bcbd97839627b4e
Lucas Sales <lucassales398@gmail.com>
2020-03-31 23:37:47 -0300

    Update ApiMessages.php

--- app/Api/ApiMessages.php
+++ app/Api/ApiMessages.php
@@ -4,7 +4,7 @@ namespace App\Api;
 
 class ApiMessages
 {
-    private $messages = [];
+    private $message = [];
     public function __construct(string $message, array $data = [])
     {
         $this->message['message'] = $message;
